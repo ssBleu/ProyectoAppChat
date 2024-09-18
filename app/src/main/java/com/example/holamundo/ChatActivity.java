92dcbe180d478a903550d52a481862741b93e11e
Bleu <loayzajhosep58@gmail.com>
2024-09-18 09:38:08 -0500

    mas layouts - correciones

--- app/src/main/java/com/example/holamundo/ChatActivity.java
+++ app/src/main/java/com/example/holamundo/ChatActivity.java
@@ -14,7 +14,7 @@ public class ChatActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_chat);
+        setContentView(R.layout.activity_chat2);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
